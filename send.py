eb4f3d5755850f4675d6837fe2696983f5f68762
Simge Nur Cankaya <simgenurcankaya@gmail.com>
2019-11-15 00:08:25 +0300

    sender python

---
+++ send.py
@@ -0,0 +1,12 @@
+import socket 
+UDP_IP = "10.10.2.1"
+UDP_PORT = 25412
+MESSAGE = "Hello, World!"
+
+print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT
+print "message:", MESSAGE
+ 
+sock = socket.socket(socket.AF_INET, # Internet
+                    socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
