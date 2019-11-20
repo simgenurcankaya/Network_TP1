a566814d01da1600e2ae6dfb753c4d06cf79f93a
Simge Nur Cankaya <simgenurcankaya@gmail.com>
2019-11-21 00:23:24 +0300

    Client server final mode working)

--- clientserverr1.py
+++ clientserverr1.py
@@ -1,8 +1,10 @@
 import socket
+import time
 
-UDP_IP_ADDRESS = "10.10.8.2"
-UDP_PORT1 = 40000  #data alma port
-UDP_PORT2 = 40001  #data yollama port
+UDP_IP_ADDRESS = "10.10.8.1"  #r2den r1e
+UDP_IP_ADDRESS2 = "10.10.8.2" #r1den r2ye
+UDP_PORT1 = 32984  #data alma port
+UDP_PORT2 = 34156  #data yollama port
 Message = "Hello, R2"
 
 clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -12,11 +14,11 @@ serverSock.bind((UDP_IP_ADDRESS, UDP_PORT1))
 
 
 for i in range(0,10):
+    #Send packets to destination 
+    clientSock.sendto(Message, (UDP_IP_ADDRESS2, UDP_PORT2))
     #Sleep to sync 
     time.sleep(1)
     for y in range (0,10):
         #Receive packets
         data, addr = serverSock.recvfrom(18) 
         print "received message:", data
-        #Send packets to destination 
-        clientSock.sendto(Message, (UDP_IP_ADDRESS, UDP_PORT2))
