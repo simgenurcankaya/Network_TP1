8a722932ccd49120949ec885a706b938dcfa48b7
MSuha <msuhademirel@gmail.com>
2019-11-21 18:07:26 +0300

    Implementation of Client&Server Function
    ClientServer function are seperated into 2 function

--- clientserverr1.py
+++ clientserverr1.py
@@ -10,49 +10,25 @@ UDP_IP_ADDRESS = "10.10.8.1"  #r2den r1e
 UDP_IP_ADDRESS2 = "10.10.8.2" #r1den r2ye
 UDP_PORT= 32984  #data alma port
 
-Message = "Hello, R2"
+Message = "This message has sent by R1"
 
 clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 serverSock.bind((UDP_IP_ADDRESS, UDP_PORT))
 
-def message(port,ip):
-    for i in range(5):
-        if(ip == UDP_IP_ADDRESS2): ## sender
-            clientSock.sendto(Message, (ip, port)) 
-        elif (ip == UDP_IP_ADDRESS): #receiver
-            data, addr = serverSock.recvfrom(18) 
-            print "received message:", data
-        else:
-            print "asdalksdaslk\n"
+def Client(port,ip):
+        logging.info("Client mode for R1")
+        clientSock.sendto(Message, (ip, port))
 
+def Server(port,ip):
+        logging.info("Server mode for R1")
+        data, addr = serverSock.recvfrom(50) 
+        print "received message:", data
 
 if __name__ == "__main__":
-    
-
-    format = "%(asctime)s: %(message)s"
-
-    logging.basicConfig(format=format, level=logging.INFO,
-
-                        datefmt="%H:%M:%S")
-
-
-    logging.info("Main    : before creating thread")
-
-    x = threading.Thread(target=message, args=(UDP_PORT,UDP_IP_ADDRESS))
-    y = threading.Thread(target=message, args=(UDP_PORT,UDP_IP_ADDRESS2))
-   
-
-    logging.info("Main    : before running thread")
-    
+    x = threading.Thread(target=Server, args=(UDP_PORT,UDP_IP_ADDRESS))
     x.start()
+    
+    y = threading.Thread(target=Client, args=(UDP_PORT,UDP_IP_ADDRESS2))
     y.start()
-
-    logging.info("Main    : wait for the thread to finish")
-
-    # x.join()
-
-    logging.info("Main    : all done")
-
-print "Thread Stopped"
--- clientserverr2.py
+++ clientserverr2.py
@@ -10,47 +10,25 @@ UDP_IP_ADDRESS = "10.10.8.1"  #r2den r1e
 UDP_IP_ADDRESS2 = "10.10.8.2" #r1den r2ye
 UDP_PORT = 32984  
 
-Message = "Hello, R1"
+Message = "This message has sent by R2"
 
 clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 serverSock.bind((UDP_IP_ADDRESS2, UDP_PORT))
 
-def message(port,ip):
-    for i in range(5):
-        if(ip == UDP_IP_ADDRESS): ## sender
-            clientSock.sendto(Message, (ip, port)) 
-        elif (ip == UDP_IP_ADDRESS2): #receiver
-            data, addr = serverSock.recvfrom(18) 
-            print "received message:", data
-        else:
-            print "siiiiisssssssssssssssssssssssssssssssadasd\n"
+def Client(port,ip):
+        logging.info("Client mode for R2")
+        clientSock.sendto(Message, (ip, port)) 
 
-
-if __name__ == "__main__":
-
-
-    format = "%(asctime)s: %(message)s"
-
-    logging.basicConfig(format=format, level=logging.INFO,
-
-                        datefmt="%H:%M:%S")
-
-
-    logging.info("Main    : before creating thread")
-
-    x = threading.Thread(target=message, args=(UDP_PORT,UDP_IP_ADDRESS))
-    y = threading.Thread(target=message, args=(UDP_PORT,UDP_IP_ADDRESS2))
-   
-
-    logging.info("Main    : before running thread")
+def Server(port,ip):
+        logging.info("Server mode for R2")
+        data, addr = serverSock.recvfrom(50) 
+        print "received message:", data
     
+if __name__ == "__main__":
+    y = threading.Thread(target=Server, args=(UDP_PORT,UDP_IP_ADDRESS2))
     y.start()
+   
+    x = threading.Thread(target=Client, args=(UDP_PORT,UDP_IP_ADDRESS))
     x.start()
-
-    logging.info("Main    : wait for the thread to finish")
-
-    # x.join()
-
-    logging.info("Main    : all done")
