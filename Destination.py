60d0d30007f61b0fffc38825870271d61a087498
MSuha <msuhademirel@gmail.com>
2019-11-21 19:56:18 +0300

    Update Destination.py

--- Destination.py
+++ Destination.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import threading
 
 IP_r1_d = "10.10.4.2" #The IP of Router1 -> Destination
 IP_r2_d = "10.10.5.2" #The IP of Router2 -> Destination
@@ -9,27 +10,31 @@ PORT_r1 = 23426 #Port Number For Router1
 PORT_r2 = 44004 #Port Number For Router2
 PORT_r3 = 44002 #Port Number For Router3
 
+serverSockR1.bind((IP_r1_d, PORT_r1))
+serverSockR2.bind((IP_r2_d, PORT_r2))
+serverSockR3.bind((IP_r3_d, PORT_r3))
+
 def message(port,ip):
     for i in range(100):
         if(ip == IP_r1_d):
-            data, addr = serverSock.recvfrom(50) 
-            print "received message:", data
+            data, addr = serverSockR1.recvfrom(50) 
+            print "received message from Router1:", data
         
         elif (ip == IP_r2_d):
-            data, addr = serverSock.recvfrom(50) 
-            print "received message:", data
+            data, addr = serverSockR2.recvfrom(50) 
+            print "received message from Router2:", data
         
         elif (ip == IP_r3_d):
-            data, addr = serverSock.recvfrom(50) 
-            print "received message:", data
+            data, addr = serverSockR3.recvfrom(50) 
+            print "received message from Router3:", data
         
         else:
             print "Error"
 
 if __name__ == "__main__":
-    thread1 = threading.Thread(target=message, args=(PORT_r1,IP_r1_s))
-    thread2 = threading.Thread(target=message, args=(PORT_r2,IP_s_r2))
-    thread3 = threading.Thread(target=message, args=(PORT_r3,IP_r3_s))
+    thread1 = threading.Thread(target=message, args=(PORT_r1,IP_r1_d))
+    thread2 = threading.Thread(target=message, args=(PORT_r2,IP_r2_d))
+    thread3 = threading.Thread(target=message, args=(PORT_r3,IP_r3_d))
 
     thread1.start()
     thread2.start()
