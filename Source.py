f9bd0a445432656fbe9a312a1d983ef60af794f2
MSuha <msuhademirel@gmail.com>
2019-11-21 19:58:53 +0300

    Update Source.py

--- Source.py
+++ Source.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import threading
 
 IP_r1_s = "10.10.1.1" #The IP of Router1 -> Source
 IP_r2_s = "10.10.2.2" #The IP of Router2 -> Source
@@ -13,23 +14,23 @@ serverSockR1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSockR2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSockR3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-serverSockR1.bind((IP_r1_s, PORT_r1))
-serverSockR2.bind((IP_r2_s, PORT_r2))
-serverSockR3.bind((IP_r3_s, PORT_r3))
+serverSockR1.bind(IP_r1_s, PORT_r1)
+serverSockR2.bind(IP_r2_s, PORT_r2)
+serverSockR3.bind(IP_r3_s, PORT_r3)
 
 def message(port,ip):
     for i in range(100):
         if(ip == IP_r1_s):
-            data, addr = serverSock.recvfrom(50) 
-            print "received message:", data
+            data, addr = serverSockR1.recvfrom(50) 
+            print "received message from R1:", data
         
         elif (ip == IP_s_r2):
-            data, addr = serverSock.recvfrom(50) 
-            print "received message:", data
+            data, addr = serverSockR2.recvfrom(50) 
+            print "received message from R2:", data
         
         elif (ip == IP_r3_s):
-            data, addr = serverSock.recvfrom(50) 
-            print "received message:", data
+            data, addr = serverSockR3.recvfrom(50) 
+            print "received message from R3:", data
         
         else:
             print "Error"
